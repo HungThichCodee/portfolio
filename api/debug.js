fff3642710c2125b05664f5819e73d8feaaee0bc
HungThichCodee <danhcaolamhung9122004@gmail.com>
2026-04-15 13:43:29 +0700

    .

--- api/debug.js
+++ api/debug.js
@@ -1,8 +1,16 @@
 export default function handler(req, res) {
+  // Show all env keys that contain "EMAIL" or start with "VERCEL"
+  const allKeys = Object.keys(process.env);
+  const emailKeys = allKeys.filter((k) => k.includes("EMAIL"));
+  const vercelKeys = allKeys.filter((k) => k.startsWith("VERCEL_"));
+
   res.json({
-    hasUsername: !!process.env.EMAIL_USERNAME,
-    hasPassword: !!process.env.EMAIL_PASSWORD,
-    usernameLength: (process.env.EMAIL_USERNAME || "").length,
-    passwordLength: (process.env.EMAIL_PASSWORD || "").length,
+    totalEnvVars: allKeys.length,
+    emailKeys,
+    vercelRegion: process.env.VERCEL_REGION || "none",
+    vercelEnv: process.env.VERCEL_ENV || "none",
+    hasEmailUsername: !!process.env.EMAIL_USERNAME,
+    hasEmailPassword: !!process.env.EMAIL_PASSWORD,
+    nodeVersion: process.version,
   });
 }
